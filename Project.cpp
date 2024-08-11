5b09abb126589c661fbc5508e9dc975e4986461c
jay patel <jpatel8384@conestoga.ca.on>
2024-08-11 16:33:32 -0400

    made Structure for each parcel and hash table.

--- Project.cpp
+++ Project.cpp
@@ -1,2 +1,19 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#pragma warning(disable : 4996)
+
+// Structure for each parcel
+typedef struct Parcel {
+    char* country;    // Dynamically allocated
+    double weight;
+    double value;
+    struct Parcel* left, * right;
+} Parcel;
+
+// Hash table with 127 buckets, each holding the root of a BST
+typedef struct HashTable {
+    Parcel* table[127];
+} HashTable;
+
+
